dcfdeab0c08bc9523909eeaa6cde3cf4fad2dfca
Valerii <valeriy.stronskiy@gmail.com>
2024-07-18 06:53:58 +0300

    Added some action logic

--- app/src/main/java/com/ua/project/android_homework_simplecalculator/MainActivity.java
+++ app/src/main/java/com/ua/project/android_homework_simplecalculator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ua.project.android_homework_simplecalculator;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     private double calcResult = 0;
     private int operand1 = 0;
     private int operand2 = 0;
+    private char currentAction = ' ';
     private ActivityMainBinding binding;
 
     @Override
@@ -20,31 +22,74 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         super.onCreate(savedInstanceState);
         setContentView(binding.getRoot());
+
+        binding.btnActionClear.setOnClickListener(v -> {
+            clearOperandsData();
+
+            calcResult = 0;
+        });
     }
 
     public void numberBtn_OnClick(View view) {
-        Button numberBtn = (Button) view;
+        if(view instanceof Button) {
+            Button numberBtn = (Button) view;
 
-        try {
-            operand1 *= 10;
-            operand1 += Integer.parseInt(numberBtn.getText().toString());
+            try {
+                operand1 *= 10;
+                operand1 += Integer.parseInt(numberBtn.getText().toString());
 
-            binding.tvOperand1.setText(String.valueOf(operand1));
-        }
-        catch (Exception ex) {
-            new AlertDialog.Builder(MainActivity.this)
-                    .setTitle("Number button exception!")
-                    .setMessage(ex.getMessage())
-                    .setPositiveButton("OK", (dialog, which) -> {
-                        dialog.dismiss();
-                    }).show();
+                binding.tvOperand1.setText(String.valueOf(operand1));
+            }
+            catch (Exception ex) {
+                displayErrorInAlertDialog(ex.getMessage());
+            }
         }
     }
 
     public void actionBtn_OnClick(View view) {
-        Button actionBtn = (Button) view;
+        if(view instanceof Button) {
+            try {
+                char prevAction = ' ';
+                Button actionBtn = (Button) view;
 
+                if(currentAction != ' ') {
+                    prevAction = currentAction;
+                }
 
+                currentAction = actionBtn.getText().charAt(0);
+
+                if(operand2 != 0 && currentAction != '=') {
+                    calcResult = calculate(currentAction);
+
+                    binding.tvOperand1.setText(String.valueOf(calcResult));
+                }
+                else if(operand2 != 0 && prevAction != ' ') {
+                    calcResult = calculate(prevAction);
+
+                    clearOperandsData();
+
+                    binding.tvOperand1.setText(String.valueOf(calcResult));
+                }
+
+                if(currentAction != '=') {
+                    operand2 = operand1;
+                    operand1 = 0;
+
+                    binding.tvOperand2.setText(String.valueOf(operand2));
+                    binding.tvOperand1.setText("0");
+                }
+            }
+            catch (Exception ex){
+                displayErrorInAlertDialog(ex.getMessage());
+            }
+        }
+    }
+
+    private void displayErrorInAlertDialog(String msg) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Number button exception!")
+                .setMessage(msg)
+                .setPositiveButton("OK", (dialog, which) -> dialog.dismiss()).show();
     }
 
     private double calculate(char action) {
@@ -57,12 +102,20 @@ public class MainActivity extends AppCompatActivity {
                 return this.operand2 * this.operand1;
             case '/':
                 if(this.operand2 == 0 || this.operand1 == 0) {
-                    throw new ArithmeticException();
+                    throw new ArithmeticException("Division by zero!");
                 }
 
-                return this.operand2 / this.operand1;
+                return (double) this.operand2 / this.operand1;
             default:
-                throw new ArithmeticException();
+                throw new ArithmeticException("Invalid action!");
         }
+    }
+
+    private void clearOperandsData() {
+        operand1 = 0;
+        operand2 = 0;
+
+        binding.tvOperand1.setText("0");
+        binding.tvOperand2.setText("0");
     }
 }
